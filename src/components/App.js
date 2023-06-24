9d2c026b579b15623aebe1f9117726df064aaa16
Polash Biswas <polashbiswasit19@gmail.com>
2023-06-24 17:27:38 +0530

    submitting code-Sat Jun 24 2023 17:27:34 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,50 @@
-
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import axios from "axios"
+
 
 const App = () => {
-  return (
+
+  let [search, setSearch] = useState("")
+  let [movies, setMovies] = useState(undefined)
+
+  function handleSearch(event) {
+      event.preventDefault()
+       axios.get("http://www.omdbapi.com",{
+          params:{
+              apikey: "873dff56",
+              s: search
+          }
+       })
+       .then(response => {
+          // console.log(response.data.Search)
+          setMovies(response.data.Search)
+          setSearch("")
+       })
+        .catch(err => console.log(err))
+  }
+   
+
+  return(
     <div>
-        {/* Do not remove the main div */}
+        <h2>Search Movie</h2>
+        <form>
+          <input type="text" placeholder="Enter Movie Name" 
+          onChange={(e)=>{setSearch(e.target.value)}} value={search}
+          />
+          <button type="submit" onClick={handleSearch}>Search</button>
+        </form>
+          <ul>
+            { 
+            movies !== undefined ?  (
+            movies.map((movie) => (
+                  <li key={movie.imdbID}>
+                        <h1>{movie.Title}</h1>
+                        <img  src={movie.Poster} alt={movie.Title}/>
+                  </li>
+              ))) : (<p className="error">Invalid movie name. Please try again.</p>)
+            }
+          </ul>
     </div>
   )
 }
